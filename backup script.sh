fd5c856899bb661b841faeb8c53211d0c2897f46
amirali108 <117827939+amirali108@users.noreply.github.com>
2023-02-20 23:04:21 +0100

    Update backup script.sh

--- backup script.sh
+++ backup script.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+   echo "Error: two arguments are required - the directory to be backed up, remote server IP and the destination directory for the backup archive."
+   exit 1
+fi
+
+source_dir=$1
+target_dir=$2
+
+if [[    ]]; then
+ # Use rsync over ssh for remote targets
+  
+
+
+
+
+  
+
+else
+  
