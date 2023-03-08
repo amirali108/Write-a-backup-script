05a957b072c8ba5c2136d5d8c7d0caa371da879a
amirali108 <117827939+amirali108@users.noreply.github.com>
2023-03-08 20:03:45 +0100

    Update backup script.sh

--- backup script.sh
+++ backup script.sh
@@ -19,16 +19,16 @@ if [[ $target_dir == *":"* ]]; then
   ssh $server "mkdir $backup_dir"
 
   rsync -avz -e "ssh -T" --link-dest=$server:$remote_dir/latest $source_dir $server:$backup_dir
- 
+
 # Update the symlink for the latest backup
   ssh $server "ln -snf $backup_dir $remote_dir/latest"
 
 else
-# Use local rsync for local targets 
+  # Use local rsync for local targets 
 
  timestamp=$(date +%Y-%m-%d_%H-%M-%S)
  backup_dir="$target_dir/$timestamp"
- 
+
  mkdir $backup_dir
 
  rsync -avz --link-dest=$target_dir/latest $source_dir $backup_dir
